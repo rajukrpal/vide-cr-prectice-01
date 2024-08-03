b5e18d09edf36ab56c6b3eebdae2ab297c6e7008
rajukrpal <rajukumarpal95@gmail.com>
2024-08-03 17:26:42 +0530

    prectice

--- src/Star.jsx
+++ src/Star.jsx
@@ -2,11 +2,18 @@ import React from "react"
 
 export default function Star(props) {
     const starIcon = props.isFilled ? "Group (1).png" : "Group.png"
+    const buttonLabel = props.isFilled ? "Unmark as favorite" : "Mark as favorite"
     return (
+        <button 
+         onClick={props.handleClick} 
+         aria-label={buttonLabel} 
+         aria-pressed={props.isFilled}
+         className="card--favorite-button" >
         <img 
             src={`${starIcon}`} 
             className="card--favorite"
-            onClick={props.handleClick}
+           alt="Star"
         />
+        </button>
     )
 }
